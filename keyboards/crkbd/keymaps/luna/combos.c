7fc55c8be843fa4835104525487af5e24be92ca4
Robert Serrano <rserranon@gmail.com>
2024-11-01 12:49:10 -0500

    Switch app change combo to DF combination

--- keyboards/crkbd/keymaps/luna/combos.c
+++ keyboards/crkbd/keymaps/luna/combos.c
@@ -1,4 +1,4 @@
-const uint16_t PROGMEM test_combo1[] = {KC_J, KC_K, COMBO_END};
+const uint16_t PROGMEM test_combo1[] = {KC_D, KC_F, COMBO_END};
 combo_t                key_combos[]  = {
     COMBO(test_combo1, RGUI(KC_SPC)), // keycodes with modifiers are possible too!
 };
